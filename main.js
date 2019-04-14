1155c2b8a3085cbd40214c78e76475832c2544d7
Nima Mahmoudi <nima_mahmoudi@live.com>
2019-04-13 18:47:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello nima-dev.com!\n");
+  response.end("Hello nima-dev.com\n");
 });
 
 // listen on localhost:8000
